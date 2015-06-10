e4e6d763053c6ccb1138758ec57ff1aa26d45051
Sword <jian-tian@126.com>
2015-06-11 00:30:21 +0800

    add new python test

--- python_practise/free_practice.py
+++ python_practise/free_practice.py
@@ -41,6 +41,67 @@ def calcFactorial():
 	t *= i
 	s += t
     print s
- 
+
+def judgeCNumber():
+    ans = ['yes', 'no']
+    i = int(raw_input('Input a number(10000-99999):'))
+    if i<10000 or i>99999:
+	print("input Error")
+    else:
+	i = str(i)
+	flag = 0
+	for j in range(0,2):
+	    if i[j] != i[4-j]:
+		flag = 1
+		break
+	print("%s" %ans[flag])
+
+def getShu():
+    a = [0]*101
+    for i in range(2,11):
+	for j in range(i+1,101,i):
+	    a[j] = -1
+    for i in range(2,101):
+	if a[i] != -1:
+	    print(' ', i) 
+
+def sortNumber():
+    print 'input 10 numbers please:'
+    l = []
+    for i in range(10):
+	l.append(int(raw_input('input a number:')))
+    for i in range(9):
+	for j in range(i+1,10):
+	    if l[j] < l[i]:
+		temp = l[j]
+		l[j] = l[i]
+		l[i] = temp
+    print l
+
+def calcTotalOfMatrix():
+    l = []
+    for i in range(3):
+	for j in range(3):
+	    l.append(int(raw_input('Input a number')))
+    s = 0
+    for i in range(3):
+	s += l[3*i+i]
+    print(s)
+
+def calcNewSort():
+    l = [0,10,20,30,40,50]
+    
+    print('the sorted list is:',l)
+    cnt = len(l)
+    n = int(raw_input('input a number:'))
+    l.append(n)
+    for i in range(cnt):
+	if n<l[i]:
+	    for j in range(cnt,i,-1):
+		l[j] = l[j-1]
+	    l[i] = n
+	    break
+    print('the new sorted list is:',l)
+
 if __name__ == "__main__":
-   calcFactorial()
+    calcNewSort() 
