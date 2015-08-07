948b8ff31bc020be22a5a2ee4f0d9d2248453c8a
Sword <jian-tian@126.com>
2015-08-07 23:40:31 +0800

    modify main.c for myShell

--- c_proc/my_shell/main.c
+++ c_proc/my_shell/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #define BUFFERSIZE 16
 
+void pipel(char *input, int li_inputlen){};
+void redirect(char *input, int li_inputlen){};
 int main(int argc, char *argv[])
 {
     char *path;
@@ -9,9 +11,13 @@ int main(int argc, char *argv[])
     int li_inputlen = 0; 
     char buffer[BUFFERSIZE] = {'\0'};
     char *input;
+    char **arg;
+    int is_bj = 0;
+    int is_back = 0;
+    int i,j,k;
 
     while (1) {
-    /*input instructior*/
+	/*input instructior*/
 	path = get_current_dir_name();
 	printf("%s>$", path);
 
@@ -21,12 +27,45 @@ int main(int argc, char *argv[])
 	    lc_char = getchar();
 	}
 
-	if (li_inputlen > BUFFERSIZE) {
-	    printf("command too long! Re-enter!");
+	if (li_inputlen >= BUFFERSIZE) {
+	    printf("command too long! Re-enter!\n");
 	    li_inputlen = 0;
 	    continue;
 	}
 	input = (char *)malloc(sizeof(char) * (li_inputlen + 1));
 	strcpy(input, buffer);
+	
+	/*profile instruction*/
+	for (i = 0,j = 0,k = 0; i <= li_inputlen; i++) {
+	    if (input[i] == '<' || input[i] == '>' || input[i] == '|') {
+		if (input[i] == '|')
+		    pipel(input, li_inputlen);
+		else
+		    redirect(input, li_inputlen);
+		is_bj = 1;
+		break;
+	    }
+	    if (input[i] == ' ' || input[i] == '\t' || input[i] == '\0') {
+		if( j == 0)
+		    continue;
+		else {
+		    buffer[j++] = '\0';
+		    arg[k] = (char *)malloc(sizeof(char) * j);
+		    strcpy(arg[k], buffer);
+		    j = 0;
+		    k++;
+		}
+	    }
+	    else {
+		if (input[i] == '&' && input[i+1] == '\0') {
+		    is_back = 1;
+		    continue;
+		}
+		buffer[j++] = input[i];
+	    }
+	}
+	for(i=0;i < j; i++)
+	    printf("arg[%d] is %s\n", i, arg[j]);
+	free(input);
     }
 }
