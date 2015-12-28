4e248d564226a02394da64e8d349e6ac5c745028
Sword <jian-tian@126.com>
2015-12-28 23:56:05 +0800

    buildcmd and buildrule

---
+++ lmosem/build/krnlbuidrule.mk
@@ -0,0 +1,26 @@
+#######################
+	krnlbuidrule自动化编译配置文件Makefile
+#######################
+
+CCSTR		= 'CC -[M] 正在构建... ' $<
+PRINTCSTR	= @echo $(CCSTR)
+
+%.krnl : $(CCBUILDPATH)%.c
+	$(CC) $(CFLAGS) -o $@ $<
+	$(PRINTCSTR)
+%.krnl : $(CCBUILDPATH)%.s
+	$(CC) $(CFLAGS) -o $@ $<
+	$(PRINTCSTR)
+%.o : $(CCBUILDPATH)%.c
+	$(CC) $(CFLAGS) -o $@ $<
+	$(PRINTCSTR)
+%.o : $(CCBUILDPATH)%.s
+	$(CC) $(CFLAGS) -o $@ $<
+	$(PRINTCSTR)
+%.lds : $(CCBUILDPATH)%.S
+	$(CC) $(CFLAGS) -o $@ $<
+	$(PRINTCSTR)
+
+%.mkh : $(CCBUILDPATH)%.S
+	$(CC) $(CFLAGS) -o $@ $<
+	$(PRINTCSTR)
